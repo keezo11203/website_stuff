3d96257f47604fe6d2fd81a37e7b323be7c98362
keezo11203 <kaitlynellis0813@gmail.com>
2023-04-29 21:28:11 -0400

    location fetching....let me cook.

--- site/script.js
+++ site/script.js
@@ -1,3 +1,14 @@
 let counter = parseInt(localStorage.getItem('counter')) || 1;
 localStorage.setItem('counter', counter + 1);
 document.getElementById('counter').textContent = counter + 'th';
+
+// Get user's location based on their IP address
+fetch('https://ipapi.co/json/')
+  .then(response => response.json())
+  .then(data => {
+    const locationMessage = `And I see you're from ${data.city}, ${data.region}. Nice place!`;
+    const messageElement = document.createElement('p');
+    messageElement.textContent = locationMessage;
+    document.querySelector('.scoreboard').appendChild(messageElement);
+  })
+  .catch(error => console.log(error));
