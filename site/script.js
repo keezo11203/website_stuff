c08584af99edee1770fe0d341e7cc3b5349263e3
keezo11203 <kaitlynellis0813@gmail.com>
2023-04-29 21:32:36 -0400

    fixing the counter and loc

--- site/script.js
+++ site/script.js
@@ -1,6 +1,18 @@
 let counter = parseInt(localStorage.getItem('counter')) || 1;
 localStorage.setItem('counter', counter + 1);
-document.getElementById('counter').textContent = counter + 'th';
+document.getElementById('counter').textContent = getCounterWithSuffix(counter);
+
+function getCounterWithSuffix(count) {
+  let suffix = 'th';
+  if (count % 10 === 1 && count % 100 !== 11) {
+    suffix = 'st';
+  } else if (count % 10 === 2 && count % 100 !== 12) {
+    suffix = 'nd';
+  } else if (count % 10 === 3 && count % 100 !== 13) {
+    suffix = 'rd';
+  }
+  return count + suffix;
+}
 
 // Get user's location based on their IP address
 fetch('https://ipapi.co/json/')
