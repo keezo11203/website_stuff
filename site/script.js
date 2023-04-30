18aa3f66015faadd879ef0735c4278bc4a739915
keezo11203 <kaitlynellis0813@gmail.com>
2023-04-29 21:43:01 -0400

    other welcome message

--- site/script.js
+++ site/script.js
@@ -2,6 +2,22 @@ let counter = parseInt(localStorage.getItem('counter')) || 1;
 localStorage.setItem('counter', counter + 1);
 document.getElementById('counter').textContent = getCounterWithSuffix(counter);
 
+// Get user's location based on their IP address
+fetch('https://ipapi.co/json/')
+  .then(response => response.json())
+  .then(data => {
+    const locationMessage = `And I see you're from ${data.city}, ${data.region}. Nice place!`;
+    const messageElement = document.createElement('p');
+    messageElement.textContent = locationMessage;
+    document.querySelector('.scoreboard').appendChild(messageElement);
+
+    const welcomeMessage = "Look around my site to get to know me more!";
+    const welcomeElement = document.createElement('p');
+    welcomeElement.textContent = welcomeMessage;
+    document.querySelector('.scoreboard').appendChild(welcomeElement);
+  })
+  .catch(error => console.log(error));
+
 function getCounterWithSuffix(count) {
   let suffix = 'th';
   if (count % 10 === 1 && count % 100 !== 11) {
@@ -13,14 +29,3 @@ function getCounterWithSuffix(count) {
   }
   return count + suffix;
 }
-
-// Get user's location based on their IP address
-fetch('https://ipapi.co/json/')
-  .then(response => response.json())
-  .then(data => {
-    const locationMessage = `And I see you're from ${data.city}, ${data.region}. Nice place!`;
-    const messageElement = document.createElement('p');
-    messageElement.textContent = locationMessage;
-    document.querySelector('.scoreboard').appendChild(messageElement);
-  })
-  .catch(error => console.log(error));
