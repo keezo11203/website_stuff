ab24b472e91cf2c57759266df8bb16a679e14b80
keezo11203 <kaitlynellis0813@gmail.com>
2023-05-01 19:05:48 -0400

    fixing .js

--- site/script.js
+++ site/script.js
@@ -13,12 +13,35 @@ fetch('https://ipapi.co/json/')
     document.querySelector('.scoreboard').appendChild(welcomeElement);
 
     // Add scroll-snapping to paragraphs in the about section
-    const paragraphs = document.querySelectorAll('main > p');
-    paragraphs.forEach(p => {
+    const paragraphs = document.querySelectorAll('.about > p');
+    paragraphs.forEach((p, i) => {
       p.style.scrollSnapAlign = 'start';
+      p.setAttribute('data-scroll-index', i + 2);
     });
   })
   .catch(error => console.log(error));
+
+window.addEventListener('scroll', () => {
+  const scrollTop = document.documentElement.scrollTop;
+  const scrollHeight = document.documentElement.scrollHeight;
+  const clientHeight = document.documentElement.clientHeight;
+  const scrollFraction = scrollTop / (scrollHeight - clientHeight);
+
+  const paragraphs = document.querySelectorAll('.about > p');
+  let currentIndex = 1;
+  for (let i = 0; i < paragraphs.length; i++) {
+    const p = paragraphs[i];
+    const index = parseInt(p.getAttribute('data-scroll-index'));
+    if (scrollFraction >= (index - 1) / paragraphs.length) {
+      currentIndex = index;
+    } else {
+      break;
+    }
+  }
+
+  const message = `You're currently viewing paragraph ${currentIndex} out of ${paragraphs.length}.`;
+  document.getElementById('scroll-message').textContent = message;
+});
 
 let counter = parseInt(localStorage.getItem('counter')) || 1;
 localStorage.setItem('counter', counter + 1);
