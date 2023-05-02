a0c328080e338a8cb3030877e0f1580d1f3dcf79
keezo11203 <kaitlynellis0813@gmail.com>
2023-05-01 20:32:34 -0400

    sigh

--- site/script.js
+++ site/script.js
@@ -13,47 +13,43 @@ fetch('https://ipapi.co/json/')
     document.querySelector('.scoreboard').appendChild(welcomeElement);
 
     // Add scroll-snapping to paragraphs in the about section
-const paragraphs = document.querySelectorAll('.about > p');
-paragraphs.forEach((p, i) => {
-  p.style.scrollSnapAlign = 'start';
-  p.setAttribute('data-scroll-index', i + 1);
-});
+    const paragraphs = document.querySelectorAll('.about > p');
+    paragraphs.forEach((p, i) => {
+      p.style.scrollSnapAlign = 'start';
+      p.setAttribute('data-scroll-index', i + 1);
+      if (i === 0) {
+        p.classList.add('current');
+      }
+    });
 
-window.addEventListener('scroll', () => {
-  const scrollTop = document.documentElement.scrollTop;
-  const scrollHeight = document.documentElement.scrollHeight;
-  const clientHeight = document.documentElement.clientHeight;
-  const scrollFraction = scrollTop / (scrollHeight - clientHeight);
+    window.addEventListener('scroll', () => {
+      let currentParagraph = document.querySelector('.about > p.current');
+      let nextParagraph = currentParagraph.nextElementSibling;
+      if (nextParagraph !== null) {
+        let rect = nextParagraph.getBoundingClientRect();
+        if (rect.top <= window.innerHeight * 0.5) {
+          currentParagraph.classList.remove('current');
+          nextParagraph.classList.add('current');
+          let currentIndex = parseInt(nextParagraph.getAttribute('data-scroll-index'));
+          const message = `You're currently viewing paragraph ${currentIndex} out of ${paragraphs.length}.`;
+          document.getElementById('scroll-message').textContent = message;
+        }
+      }
+    });
 
-  let currentIndex = 1;
-  for (let i = 0; i < paragraphs.length; i++) {
-    const p = paragraphs[i];
-    const index = parseInt(p.getAttribute('data-scroll-index'));
-    if (scrollFraction >= (index - 1) / paragraphs.length) {
-      currentIndex = index;
-    } else {
-      break;
+    let counter = parseInt(localStorage.getItem('counter')) || 1;
+    localStorage.setItem('counter', counter + 1);
+    document.getElementById('counter').textContent = getCounterWithSuffix(counter);
+
+    function getCounterWithSuffix(count) {
+      let suffix = 'th';
+      if (count % 10 === 1 && count % 100 !== 11) {
+        suffix = 'st';
+      } else if (count % 10 === 2 && count % 100 !== 12) {
+        suffix = 'nd';
+      } else if (count % 10 === 3 && count % 100 !== 13) {
+        suffix = 'rd';
+      }
+      return count + suffix;
     }
-  }
-
-  const message = `You're currently viewing paragraph ${currentIndex} out of ${paragraphs.length}.`;
-  document.getElementById('scroll-message').textContent = message;
-});
-
-
-let counter = parseInt(localStorage.getItem('counter')) || 1;
-localStorage.setItem('counter', counter + 1);
-document.getElementById('counter').textContent = getCounterWithSuffix(counter);
-
-function getCounterWithSuffix(count) {
-  let suffix = 'th';
-  if (count % 10 === 1 && count % 100 !== 11) {
-    suffix = 'st';
-  } else if (count % 10 === 2 && count % 100 !== 12) {
-    suffix = 'nd';
-  } else if (count % 10 === 3 && count % 100 !== 13) {
-    suffix = 'rd';
-  }
-  return count + suffix;
-}
   });
