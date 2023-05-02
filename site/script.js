ffcf5dfb97d14372f3c0c33f143c74272c22ed71
keezo11203 <kaitlynellis0813@gmail.com>
2023-05-01 20:26:35 -0400

    yeahhhh

--- site/script.js
+++ site/script.js
@@ -13,13 +13,11 @@ fetch('https://ipapi.co/json/')
     document.querySelector('.scoreboard').appendChild(welcomeElement);
 
     // Add scroll-snapping to paragraphs in the about section
-    const paragraphs = document.querySelectorAll('.about > p');
-    paragraphs.forEach((p, i) => {
-      p.style.scrollSnapAlign = 'start';
-      p.setAttribute('data-scroll-index', i + 2);
-    });
-  })
-  .catch(error => console.log(error));
+const paragraphs = document.querySelectorAll('.about > p');
+paragraphs.forEach((p, i) => {
+  p.style.scrollSnapAlign = 'start';
+  p.setAttribute('data-scroll-index', i + 1);
+});
 
 window.addEventListener('scroll', () => {
   const scrollTop = document.documentElement.scrollTop;
@@ -27,21 +25,21 @@ window.addEventListener('scroll', () => {
   const clientHeight = document.documentElement.clientHeight;
   const scrollFraction = scrollTop / (scrollHeight - clientHeight);
 
-  const paragraphs = document.querySelectorAll('.about > p');
   let currentIndex = 1;
   for (let i = 0; i < paragraphs.length; i++) {
     const p = paragraphs[i];
     const index = parseInt(p.getAttribute('data-scroll-index'));
-    if (scrollFraction >= (index - 2) / paragraphs.length) {
+    if (scrollFraction >= (index - 1) / paragraphs.length) {
       currentIndex = index;
     } else {
       break;
     }
   }
 
-  const message = `You're currently viewing paragraph ${currentIndex - 1} out of ${paragraphs.length}.`;
+  const message = `You're currently viewing paragraph ${currentIndex} out of ${paragraphs.length}.`;
   document.getElementById('scroll-message').textContent = message;
 });
+
 
 let counter = parseInt(localStorage.getItem('counter')) || 1;
 localStorage.setItem('counter', counter + 1);
